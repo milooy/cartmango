969fe6f1c9397f723ca8a6204babc2f22dc82953
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-09-05 16:37:58 +0900

    Seperate settings file

--- cart/settings/base.py
+++ cart/settings/base.py
@@ -37,11 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
--- cart/wsgi.py
+++ cart/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cart.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cart.settings.local")
 
 application = get_wsgi_application()
