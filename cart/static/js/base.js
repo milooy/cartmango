39cdee40782847c519786db1f1dbfcf8351e592b
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-09-07 14:25:19 +0900

    list height, navbar transition

--- cart/static/js/base.js
+++ cart/static/js/base.js
@@ -3,7 +3,7 @@ $('.button-collapse').sideNav();
 
 /*-- 헤더가 보일 떈 nav를 투명하게 한다 --*/
 $(window).scroll(function() {
-    if(document.body.scrollTop>=180) {
+    if(document.body.scrollTop>=55) {
         $('.nav-main').removeClass('nav-transparent')
     } else {
         $('.nav-main').addClass('nav-transparent');
