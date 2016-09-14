9a792017e505987b829cd6ab785be7db575d50ec
yoolmoo <yoolmoo@smartstudy.co.kr>
2016-09-14 16:21:21 +0900

    #20 이미 avatar가 있으면 긁어오지 말기

--- accounts/social.py
+++ accounts/social.py
@@ -7,15 +7,11 @@ USER_FIELDS = ['email', 'nickname']
 
 
 def create_user(strategy, details, user=None, *args, **kwargs):
-    print(details)
     if user:
         return {'is_new': False}
-
     fields = {'email': details.get('email'), 'nickname': details.get('username')}
-
     if not fields:
         return
-
     return {
         'is_new': True,
         'user': strategy.create_user(**fields)
@@ -24,7 +20,9 @@ def create_user(strategy, details, user=None, *args, **kwargs):
 
 def update_avatar(backend, response, uid, user, *args, **kwargs):
     if backend.name == 'facebook':
-        url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
-        avatar = urlopen(url)
-        user.avatar.save(slugify(user.email + "_social") + '.jpg', ContentFile(avatar.read()))
-        user.save()
+        if not user.avatar:
+            url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
+            avatar = urlopen(url)
+            user.avatar.save(slugify(user.email + "_social") + '.jpg',
+                             ContentFile(avatar.read()))
+            user.save()
