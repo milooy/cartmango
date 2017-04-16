e6139381ab8e5328a61d45186a32d8243620c3c3
JayJin <jayjinjay@gmail.com>
2017-04-17 01:05:33 +0900

    #9 제품 정보 긁어오는거 함수로 빼기

--- extension/popup.js
+++ extension/popup.js
@@ -1,30 +1,74 @@
-var storage = chrome.storage.local;
+
 
 function getDataFromDOM() {
   var product = {};
-  product.title = document.querySelector('meta[property="og:title"]').content;
-  product.url = document.querySelector('meta[property="og:url"]').content;
-  product.price = 300;
-  product.shop = document.querySelector('meta[property="og:site_name"]').content;
-  product.img = document.querySelector('meta[property="og:image"]').content;
+  function getTitle() {
+    var title = $('meta[property="og:title"]').attr('content') || $('meta[name="title"]').attr('content');
+    if(title) {
+      return title;
+    }
+    return '타이틀 몰라';
+  }
+
+  function getUrl() {
+    var url = $('meta[property="og:url"]').attr('content');
+    if(url) {
+      return url;
+    }
+    return window.location.href;
+  }
+
+  function getPrice() {
+    return 300;
+  }
+
+  function getShop() {
+    var shop = $('meta[property="og:site_name"]').attr('content');
+    if(shop) {
+      return shop;
+    }
+    var host = window.location.host;
+    return host.startsWith('www.')? host.substring(4) : host;
+  }
+
+  function getImg() {
+    var img = $('meta[property="og:image"]').attr('content');
+    if(img) {
+      if(img.startsWith("//")) {
+        return "http://" + img;
+      }
+      return img;
+    }
+    return '';
+  }
+
+  product.title = getTitle();
+  product.url = getUrl();
+  product.price = getPrice();
+  product.shop = getShop();
+  product.img = getImg();
+
   console.log("product in js", product)
   return product;
 }
 
 $(function() {
+  var storage = chrome.storage.local;
   var pageUrl = chrome.extension.getURL('page.html');
   var product = null;
+  chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+    chrome.tabs.executeScript({ code: '(' + getDataFromDOM + ')();' }, (results) => {
+      product = results[0];
+      product.timestamp = Math.floor(Date.now() / 1000);
 
-  chrome.tabs.executeScript({ code: '(' + getDataFromDOM + ')();' }, (results) => {
-    product = results[0];
-    product.timestamp = Math.floor(Date.now() / 1000);
-
-    $('#product_title').text(product.title);
-    $('#product_url').text(product.url);
-    $('#product_price').text(product.price);
-    $('#product_shop').text(product.shop);
-    $('#product_img').attr('src', product.img);
+      $('#product_title').text(product.title);
+      $('#product_url').text(product.url);
+      $('#product_price').text(product.price);
+      $('#product_shop').text(product.shop);
+      $('#product_img').attr('src', product.img);
+    });
   });
+
 
   $('#save_cart').click(function() {
     chrome.storage.sync.get('product_list', function(items) {
